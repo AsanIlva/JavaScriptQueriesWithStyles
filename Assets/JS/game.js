66db291044f338b10b09fd8390e8b8a6019f4739
Pasan De Silva <pasanderaw1212@gmail.com>
2021-10-03 23:01:29 +1030

    5th commit

--- Assets/JS/game.js
+++ Assets/JS/game.js
@@ -10,36 +10,43 @@ let questionCounter = 0;
 let availableQuesions = [];
 
 let questions = [
-  {
-    question: "Inside which HTML element do we put the JavaScript??",
-    choice1: "<script>",
-    choice2: "<javascript>",
-    choice3: "<js>",
-    choice4: "<scripting>",
-    answer: 1
-  },
-  {
-    question:
-      "What is the correct syntax for referring to an external script called 'xxx.js'?",
-    choice1: "<script href='xxx.js'>",
-    choice2: "<script name='xxx.js'>",
-    choice3: "<script src='xxx.js'>",
-    choice4: "<script file='xxx.js'>",
-    answer: 3
-  },
-  {
-    question: " How do you write 'Hello World' in an alert box?",
-    choice1: "msgBox('Hello World');",
-    choice2: "alertBox('Hello World');",
-    choice3: "msg('Hello World');",
-    choice4: "alert('Hello World');",
-    answer: 4
-  }
+    {
+        question: "What type of language is JavaScript",
+        choice1: "Object-Oriented",
+        choice2: "Object-Based",
+        choice3: "Assembly-Language",
+        Choice4: "High-Level",
+        answer: 2
+    },
+    {
+        question: "What is 'function' and 'var' are known as?",
+        choice1: "Keywords",
+        choice2: "Data types",
+        choice3: "Declaration statements",
+        choice4: "Prototypes",
+        answer: 3   
+    },
+    {
+        question: "Which of the following variables takes precedence over the others if the names are the same?",
+        choice1: "Global variable",
+        choice2: "The local element",
+        choice3: "The two of the above",
+        choice4: "None of the above",
+        answer: 2
+    },
+    {
+      question: "In the JavaScript, which one of the following is not considered as an error?",
+      choice1: "Syntax error",
+      choice2: "Missing of semicolons",
+      choice3: "Division by zero",
+      choice4: "Missing of Bracket",
+      answer: 3 
+    }
 ];
 
 //CONSTANTS
 const CORRECT_BONUS = 10;
-const MAX_QUESTIONS = 3;
+const MAX_QUESTIONS = 4;
 
 startGame = () => {
   questionCounter = 0;
@@ -50,12 +57,13 @@ startGame = () => {
 
 getNewQuestion = () => {
   if (availableQuesions.length === 0 || questionCounter >= MAX_QUESTIONS) {
-    localStorage.setItem("mostRecentScore", score);
+      localStorage.setItem("mostRecentScore", score);
     //go to the end page
     return window.location.assign("end.html");
   }
   questionCounter++;
   progressText.innerText = `Question ${questionCounter}/${MAX_QUESTIONS}`;
+  //"or"questionCounter + "/" + MAX_QUESTIONS;
   //Update the progress bar
   progressBarFull.style.width = `${(questionCounter / MAX_QUESTIONS) * 100}%`;
 
